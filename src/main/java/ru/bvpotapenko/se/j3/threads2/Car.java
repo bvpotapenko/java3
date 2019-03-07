1d7bba93b7ec0221ea1b903111b6d988df0da885
bvpotapenko <bvpotapenko@gmail.com>
2019-03-07 14:03:39 +0800

    HW is ready

--- src/main/java/ru/bvpotapenko/se/j3/threads2/Car.java
+++ src/main/java/ru/bvpotapenko/se/j3/threads2/Car.java
@@ -1,24 +1,31 @@
 package ru.bvpotapenko.se.j3.threads2;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
-    private static int CARS_COUNT; // FIXME: 07-Mar-19 ??
+    private static int CARS_COUNT;
     static {
         CARS_COUNT = 0;
     }
     private Race race;
     private int speed;
     private String name;
+    private Starter starter;
+    private CountDownLatch finish;
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, Starter starter, CountDownLatch finish) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.starter = starter;
+        this.finish = finish;
     }
     @Override
     public void run() {
@@ -26,11 +33,14 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            starter.getCb().await();
+            starter.startCommand().await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        finish.countDown();
     }
 }
--- src/main/java/ru/bvpotapenko/se/j3/threads2/Main.java
+++ src/main/java/ru/bvpotapenko/se/j3/threads2/Main.java
@@ -1,18 +1,34 @@
 package ru.bvpotapenko.se.j3.threads2;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+
 public class Main {
     public static final int CARS_COUNT = 4;
+
     public static void main(String[] args) {
         System.out.println("IMPORTANT NOTICE >>> Prepare!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        final CountDownLatch finish = new CountDownLatch(CARS_COUNT);
+        Race race = new Race(new Road(60), new Tunnel(CARS_COUNT / 2), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+        Starter starter = new Starter(CARS_COUNT);
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), starter, finish);
         }
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("IMPORTANT NOTICE >>> The Race has started!!!");
-        System.out.println("IMPORTANT NOTICE >>> The Race is over!!!");
+        try {
+            starter.getCb().await();
+            System.out.println("IMPORTANT NOTICE >>> The Race has started!!!");
+            starter.startCommand().countDown();
+            finish.await();
+            System.out.println("IMPORTANT NOTICE >>> The Race is over!!!");
+        } catch (InterruptedException | BrokenBarrierException e) {
+            System.out.println("IMPORTANT NOTICE >>> The Race was canceled!!!");
+            e.printStackTrace();
+        }
+
     }
 }
+
